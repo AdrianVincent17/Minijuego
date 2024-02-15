8e31f80186ae978d5e29aaad0a80381d7e6177b7
Adrian Vicente <adrian.vicente.lopez@ces-vegamedia.es>
2024-02-15 09:37:59 +0100

    Primera clase padre Personaje y mitad de la calse Link

---
+++ VegaJuego/Link.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VegaJuego
+{
+    internal class Link : Personaje
+    {
+        private int salud;
+        private int escudo;
+        private int arma;
+    }
+    public Link(Level1 vegaZelda, int x, int  y, int s, int e, int a) : base(vegaZelda, x, y)
+    {
+        salud = s;
+        escudo = e;
+        arma = a;
+    }
+}
---
+++ VegaJuego/personaje.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VegaJuego
+{
+    internal class Personaje
+    {
+        private int coordx;
+        private int coordy;
+        private Level1 lienzo;
+        private PictureBox caja= new PictureBox();
+        private String direccion;
+
+        public Personaje(Level1 vegaZelda, int x, int y)
+        {
+            coordx = x;
+            coordy = y;
+            lienzo = vegaZelda;
+            direccion = "Derecha";
+            caja.Location=new System.Drawing.Point(Coordx, Coordy);
+            caja.BackColor = System.Drawing.Color.Transparent;
+            caja.Size = new System.Drawing.Size(110, 110);
+            caja.BackgroundImageLayout=System.Windows.Forms.ImageLayout.None;
+            caja.BackgroundImage = global::VegaJuego.Properties.Resources.derecha;
+
+            vegaZelda.Controls.Add(caja); caja.Visible = true;
+        }
+        public String Direccion
+        {
+            get { return direccion; }
+            set { direccion = value; }
+        }
+        public int Coordy
+        {
+            get { return coordy; }
+            set { coordy = value; }
+        }
+        public int Coordx
+        {
+            get { return coordx; }
+            set { coordx = value; }
+        }
+
+        public Level1 Lienzo
+        {
+            get { return lienzo; }
+            set { lienzo = value; }
+        }
+
+        public PictureBox Caja
+        {
+            get { return caja; }
+            set { caja = value; }
+        }
+        //Metodo que se definira en cada clase hija
+            public void mover()
+        {
+
+        }
+        }
+    }
+
