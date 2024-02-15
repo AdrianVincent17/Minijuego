d595634102d5cdf335b107a0755141d2122dd24c
Adrian Vicente <adrian.vicente.lopez@ces-vegamedia.es>
2024-02-15 10:14:59 +0100

    terminada la clase Link

--- VegaJuego/Link.cs
+++ VegaJuego/Link.cs
@@ -12,11 +12,30 @@ namespace VegaJuego
         private int salud;
         private int escudo;
         private int arma;
-    }
-    public Link(Level1 vegaZelda, int x, int  y, int s, int e, int a) : base(vegaZelda, x, y)
-    {
-        salud = s;
-        escudo = e;
-        arma = a;
+
+        public Link(Level1 vegaZelda, int x, int y, int s, int e, int a) : base(vegaZelda, x, y)
+        {
+            salud = s;
+            escudo = e;
+            arma = a;
+        }
+
+        public int Arma
+        {
+            get { return Arma; }
+            set { arma = value; }
+        }
+
+        public int Escudo
+        {
+            get { return Escudo; }
+            set { escudo = value; }
+        }
+
+        public int Salud
+        {
+            get { return Salud; }
+            set { salud = value; }
+        }
     }
 }
