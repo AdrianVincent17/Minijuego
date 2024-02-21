d8aeb0f1eb127899ed7026d1cfd2189f17f62729
AdrianVincent17 <adrian.vicente.lopez@ces-vegamedia.es>
2024-02-21 21:18:40 +0100

    añadidos iconos de vida, escudo, arma con sus respectivos contadores

--- VegaJuego/Level1.Designer.cs
+++ VegaJuego/Level1.Designer.cs
@@ -28,8 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Level1));
+            MovEnemigos = new System.Windows.Forms.Timer(components);
+            labelVida = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            labelEscudo = new Label();
+            labelArma = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
             SuspendLayout();
+            // 
+            // MovEnemigos
+            // 
+            MovEnemigos.Enabled = true;
+            MovEnemigos.Tick += timer1_Tick;
+            // 
+            // labelVida
+            // 
+            labelVida.AutoSize = true;
+            labelVida.BackColor = Color.Transparent;
+            labelVida.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            labelVida.ForeColor = Color.Gold;
+            labelVida.Location = new Point(620, 125);
+            labelVida.Name = "labelVida";
+            labelVida.Size = new Size(108, 54);
+            labelVida.TabIndex = 0;
+            labelVida.Text = "Vida";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.BackgroundImage = Properties.Resources.salud;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Center;
+            pictureBox1.Location = new Point(586, 125);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(43, 39);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.Transparent;
+            pictureBox2.BackgroundImage = Properties.Resources.escudo;
+            pictureBox2.BackgroundImageLayout = ImageLayout.Center;
+            pictureBox2.Location = new Point(586, 175);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(43, 39);
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = Color.Transparent;
+            pictureBox3.BackgroundImage = Properties.Resources.Arma;
+            pictureBox3.BackgroundImageLayout = ImageLayout.Center;
+            pictureBox3.Location = new Point(586, 225);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(43, 39);
+            pictureBox3.TabIndex = 1;
+            pictureBox3.TabStop = false;
+            // 
+            // labelEscudo
+            // 
+            labelEscudo.AutoSize = true;
+            labelEscudo.BackColor = Color.Transparent;
+            labelEscudo.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            labelEscudo.ForeColor = Color.Gold;
+            labelEscudo.Location = new Point(620, 175);
+            labelEscudo.Name = "labelEscudo";
+            labelEscudo.Size = new Size(154, 54);
+            labelEscudo.TabIndex = 0;
+            labelEscudo.Text = "Escudo";
+            // 
+            // labelArma
+            // 
+            labelArma.AutoSize = true;
+            labelArma.BackColor = Color.Transparent;
+            labelArma.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            labelArma.ForeColor = Color.Gold;
+            labelArma.Location = new Point(620, 225);
+            labelArma.Name = "labelArma";
+            labelArma.Size = new Size(126, 54);
+            labelArma.TabIndex = 0;
+            labelArma.Text = "Arma";
             // 
             // Level1
             // 
@@ -37,6 +122,12 @@
             BackgroundImage = Properties.Resources.FondoFinal;
             BackgroundImageLayout = ImageLayout.Stretch;
             ClientSize = new Size(786, 403);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(labelArma);
+            Controls.Add(labelEscudo);
+            Controls.Add(labelVida);
             DoubleBuffered = true;
             Icon = (Icon)resources.GetObject("$this.Icon");
             MaximizeBox = false;
@@ -44,9 +135,21 @@
             StartPosition = FormStartPosition.CenterScreen;
             Text = "VegaZelda";
             KeyPress += Level1_KeyPress;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer MovEnemigos;
+        private Label labelVida;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private Label labelEscudo;
+        private Label labelArma;
     }
 }
